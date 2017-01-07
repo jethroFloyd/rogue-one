aaf5e5b564111f76d888b2b38573994361b60f6b
Ritobroto Maitra <ritobrotomaitra@gmail.com>
2017-01-08 02:15:37 +0530

    Minified and Localized

--- js/index2.js
+++ js/index2.js
@@ -11,7 +11,3 @@
 })();
 
 
-/**
-Copyright 2016 Google Inc. All Rights Reserved. 
-Use of this source code is governed by an MIT-style license that can be foundin the LICENSE file at http://material.angularjs.org/HEAD/license.
-**/
--- js/index3.js
+++ js/index3.js
@@ -25,8 +25,3 @@ angular.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
   $scope.isDisabled = true;
 });
 
-
-/**
-Copyright 2016 Google Inc. All Rights Reserved. 
-Use of this source code is governed by an MIT-style license that can be foundin the LICENSE file at http://material.angularjs.org/HEAD/license.
-**/
